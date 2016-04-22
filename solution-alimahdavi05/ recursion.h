7cfb99ffe8bd47f284cde28b6dba25f8f2d94401
alimahdavi05 <alimahdavi05@gmail.com>
2016-04-21 21:18:59 -0700

    recursion.h

---
+++ solution-alimahdavi05/ recursion.h
@@ -0,0 +1,26 @@
+
+#ifndef RECURSION_H
+#define RECURSION_H
+// ----------------------------------------------------------------------------
+
+#include <string>
+
+// ----------------------------------------------------------------------------
+
+int gcd(int a, int b);  // returns the greatest common denominator
+int fib(int n);         // returns the nth fibonacci number (starting with 1, 1)
+int pow(int a, int b);  // returns a raised to b (for positive b)
+int tri(int n);         // returns the nth triangular number
+
+int gcd_iter(int a, int b);  // (see above)
+int fib_iter(int n);         // (see above)
+int pow_iter(int a, int b);  // (see above)
+int tri_iter(int n);         // (see above)
+
+std::string int_to_roman(int n);  // returns n in roman numerals
+std::string int_to_words(int n);  // returns n in english
+std::string magic_number(int n);  // ( it's a secret :-) )
+
+// ----------------------------------------------------------------------------
+#endif
+
